d56396494dffafcfbe3a148227de0a4cc4f6282c
John Keith <johnkeith@outlook.com>
2014-07-10 15:23:41 -0400

    add full name to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     sequence(:uid) { |n| "123456#{n}" }
     provider "github"
     username "ghuser"
+    full_name "Johnny Github"
     avatar_url "http://www.example.com/ghuser.jpg"
     email "ghuser@github.com"
     access_token "123456788999000"
