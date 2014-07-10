fd7ca236f2d3454c292cc015a3aa11da6c5949df
John Keith <johnkeith@outlook.com>
2014-07-10 15:10:45 -0400

    full name migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_omniauth(auth)
     provider = auth.provider
     uid = auth.uid
-
+    binding.pry
     find_by(provider: provider, uid: uid) || create_from_omniauth(auth)
   end
 
@@ -16,8 +16,10 @@ class User < ActiveRecord::Base
       uid: auth.uid,
       email: auth.info.email,
       username: auth.info.nickname,
+      full_name: auth.info.name,
       avatar_url: auth.info.image,
       access_token: auth.credentials.token
+      websites: auth.info.urls
     )
   end
 end
