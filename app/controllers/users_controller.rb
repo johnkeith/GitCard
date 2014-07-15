1c8bdf7e00f8b558efe7ab0363365127156857cd
John Keith <johnkeith@outlook.com>
2014-07-15 11:55:05 -0400

    method to validate user submitted urls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,25 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:description, :twitter_url,
+    safe_params = params.require(:user).permit(:description, :twitter_url,
       :facebook_url, :github_url, :linkedin_url, :personal_site_url)
+    %w(twitter_url facebook_url github_url linkedin_url
+      personal_site_url).each do |site|
+        unless safe_params[site] == ""
+          safe_params[site] = validate_url(safe_params[site])
+        end
+      end
+    safe_params
+  end
+
+  def validate_url(url)
+    url = url.downcase
+    if url.include?("http://www.") || url.include?("https://www.")
+      url
+    elsif url.include?("www.")
+      "http://" + url
+    elsif !url.include?("http://www.") || !url.include?("https://www.")
+      "http://www." + url
+    end
   end
 end
